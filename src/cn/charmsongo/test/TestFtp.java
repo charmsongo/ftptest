3a330deb6d046d5398e9a30f2595ee846b52c5d7
charmsongo <charmsongo@163.com>
2020-06-08 22:04:55 +0800

    add 6.8.22

--- src/cn/charmsongo/test/TestFtp.java
+++ src/cn/charmsongo/test/TestFtp.java
@@ -6,7 +6,6 @@ public class TestFtp {
 	
 	public static void main(String[] args) {
 		boolean rv = new FtpTool().login("192.168.3.161",21,"song","song");
-		//System.out.println(rv);
 		System.out.println(rv);
 	}
 	
