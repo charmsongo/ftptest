b3638e11704779b0482c3c6bc3af3a0865f3e34c
charmsongo <charmsongo@163.com>
2020-06-08 21:51:45 +0800

    no message

--- src/cn/charmsongo/test/TestFtp.java
+++ src/cn/charmsongo/test/TestFtp.java
@@ -7,6 +7,7 @@ public class TestFtp {
 	public static void main(String[] args) {
 		boolean rv = new FtpTool().login("192.168.3.161",21,"song","song");
 		System.out.println(rv);
+		System.out.println(rv);
 	}
 	
 
